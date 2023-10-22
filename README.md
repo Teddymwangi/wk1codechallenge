1bf02c0ad818ae4fef700324a8539326607cde0d
TED <teddy.maina@student.moringaschool.com>
2023-10-22 19:07:43 +0300

    completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# wk1codechallenge
+# wk1codechallenge this code is to simply execute functions.
---
+++ code1.js
@@ -0,0 +1,6 @@
+//this code is to calculate students marks and grading them
+function calculateStudentsGrade( ) {
+    if (marks >=79  grade='A');
+}
+else if(marks >=)
+console.log return(grade)
